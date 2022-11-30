5a01f86dd67ea8c01f1341685d87efabdc7304c3
aitronnakorn <s6230613003@phuket.psu.ac.th>
2022-11-30 19:30:01 +0700

    Refirect first page

--- next.config.js
+++ next.config.js
@@ -12,6 +12,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
